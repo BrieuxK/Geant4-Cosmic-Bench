762179e47d5fd62443f7d16af4cce51c644c4659
pewlt <98036882+pewlt@users.noreply.github.com>
2023-04-20 15:53:00 +0200

    Update SlabHit.hh

--- SlabHit.hh
+++ SlabHit.hh
@@ -1,27 +1,35 @@
 #ifndef SLABHIT_HH
 #define SLABHIT_HH
 
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+#include "G4Threading.hh"
+
+
 class SlabHit
 {
     public:
         
-        double GetEdep() {return fEdep;};
+        G4double GetEdep() {return fEdep;};
         void SetEdep(double edep) {fEdep = edep;};
         
-        double GetHitTime() {return hitTime;};
+        G4double GetHitTime() {return hitTime;};
         void SetHitTime(double hit_time) {hitTime = hit_time;};
         
-        int GetChannelNbr() {return channelNbr;};
-        void SetChannelNbr(int channel_number) {channelNbr = channel_number;};
+        G4int GetChannelNbr() {return channelNbr;};
+        void SetChannelNbr(G4int channel_number) {channelNbr = channel_number;};
         
-        int GetPanelNbr() {return panelNbr;};
-        void SetPanelNbr(int panel_number) {panelNbr = panel_number;};
+        G4int GetPanelNbr() {return panelNbr;};
+        void SetPanelNbr(G4int panel_number) {panelNbr = panel_number;};
     
     private:
-        double fEdep; // Energy deposition
-        double hitTime; 
-        int channelNbr;
-        int panelNbr;
+        G4double fEdep; // Energy deposition
+        G4double hitTime; 
+        G4int channelNbr;
+        G4int panelNbr;
     
 };
 
